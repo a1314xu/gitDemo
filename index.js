da3759289d9db0cd98c7c88217457ce5c5d04666
xu_pc <xu_pc@Ctrip.com>
2016-06-12 19:11:12 +0800

    day

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-var server = require('./nodeTest');
+var server = require('./server');
+var requestHandlers = require("./requestHandlers");
+var router = require('./router');
 
-server.start();
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+server.start(router.route, handle);
